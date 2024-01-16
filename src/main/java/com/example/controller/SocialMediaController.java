1aa690a30e384fed6a07f406d96919169ef5786c
Alvaro SalinasYavi <salinasyavi@gmail.com>
2024-01-16 00:27:46 +0000

    AccountRepository, AccountService, SocialMediaController: User Registration

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,16 @@
 package com.example.controller;
 
+import com.example.entity.Account;
+import com.example.service.AccountService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.web.server.ResponseStatusException;
+
+
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +18,25 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+
+    @Autowired
+    private AccountService accountService;
+
+@PostMapping(value = "/register")
+@ResponseBody
+public Account postAccount(@RequestBody Account account){
+    Account newAccount = accountService.persistAccount(account);
+
+    if(newAccount == null){
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid username or password.");
+    } else if(newAccount.getUsername() == null){
+        throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists.");
+    }
+
+    return newAccount;
+
+}
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,35 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+    
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account persistAccount(Account account){
+        if(!account.getUsername().trim().isBlank() && account.getPassword().trim().length() > 3 ){
+            if(accountRepository.findAccountByUserName(account.getUsername()) == null){
+                return accountRepository.save(account);
+            } else{
+                return new Account();
+            }
+
+        }
+
+        return null;
+    }
+    
 }
